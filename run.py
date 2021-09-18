24aab5b98b8f1e757b2c4a059e2ca4fb669024c0
ElyseRH <elyserhoran@gmail.com>
2021-09-18 10:06:15 +0000

    Add class for Barbarian and basic roll function

--- run.py
+++ run.py
@@ -1,19 +1,16 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-import math
+import random
 
 def choose_character():
     """
     Ask user to choose character and assign it to user.
     """
-    #intro = open("intro.txt")
-    #intro_text = intro.read()
-    #intro.close()
-    #print(intro_text)
 
-    with open("story-files/intro.txt") as intro:
+    with open("./assets/story-files/intro.txt") as intro:
         intro_text = intro.read()
         print(intro_text)
+        
     print("1 - Barbarian")
     print("2 - Bard")
     print("3 - Rogue")
@@ -35,11 +32,74 @@ def choose_character():
     return user_character
 
 
+class Barbarian:
+    """
+    Barbarian stats
+    """
+    #quest = 2
+    #penny = -3
+
+    def __init__(self, path_one, path_two):
+        self.path_one = path_one 
+        self.path_two = path_two 
+
+def dice_roll(character):
+    """
+    Dice roll
+    """
+    roll = random.randrange(21)
+    print(roll)
+    #if roll >= 11:
+        #succeed/user choice
+    #else:
+        #fail
+
+def quest_or_penny(character):
+    """
+    Function for path quest_or_penny
+    """
+
+    # read txt
+    # offer choice
+    # roll
+    # succeed or fail
+    # add or subtract user character stats
+    # get final number
+    # take path
+    
+    with open("./assets/story-files/quest-or-penny.txt") as qp:
+        qp_text = qp.read()
+        print(qp_text)
+
+    qp_choice = input("Please enter your choice here; type the number for the path you want to take:\n")
+
+    #dice_roll(character)
+
+    qp_barbarian = Barbarian(5, -3)
+
+    roll = random.randrange(21)
+    print(roll)
+
+    if qp_choice == "1":
+        roll = qp_barbarian.path_one + roll
+        print(roll)
+    elif qp_choice == "2":
+        roll = qp_barbarian.path_one + roll
+        print(roll)
+
+    
+    
+
+
 def main():
     """
     Run all program functions.
     """
-    choose_character()
+    
+   
+    character = choose_character()
+    #dice_roll(character)
+    quest_or_penny(character)
 
 
 main()
