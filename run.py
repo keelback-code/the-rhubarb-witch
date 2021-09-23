9cd9eebda20848f860e0ced9e538fb45ecec60c8
ElyseRH <elyserhoran@gmail.com>
2021-09-23 11:47:42 +0000

    Add user error handler for choose_path and func parameters needed.

--- run.py
+++ run.py
@@ -1,11 +1,39 @@
 import random
 
+class Barbarian:
+    """
+    Barbarian class to be used by functions
+    to determine which stats to use.
+    """
+    def __init__(self, path_one, path_two):
+        self.path_one = path_one 
+        self.path_two = path_two
+
+
+class Rogue:
+    """
+    Rogue class to be used by functions
+    to determine which stats to use.
+    """
+    def __init__(self, path_one, path_two):
+        self.path_one = path_one 
+        self.path_two = path_two
+
+
+class Sorcerer:
+    """
+    Sorcerer class to be used by functions
+    to determine which stats to use.
+    """
+    def __init__(self, path_one, path_two):
+        self.path_one = path_one 
+        self.path_two = path_two
+
 
 def choose_character():
     """
     Introduce the game, ask user to choose character and assign it to user.
     """
-
     read_file("./assets/story-files/intro.txt")
     user_choice = input("Please enter your choice here; type the number for the character you want to select:\n")
 
@@ -39,36 +67,18 @@ def user_name():
 
     print(f"Welcome {user}! Your new name is {user_final_name}. Let's play.\n")
 
-    return user_final_name
+    return user_final_name #remove this if not used
 
 
-class Barbarian:
+def main_game_play(character, stats, current_path, first_path_initiate, second_path_initiate):
     """
-    Barbarian class to be used by functions
+    Function containing functions which most pathways
+    call. Please refer to each function for individual purpose.
     """
-    def __init__(self, path_one, path_two):
-        self.path_one = path_one 
-        self.path_two = path_two
-
-
-class Rogue:
-    """
-    Rogue class to be used by functions
-    """
-
-    def __init__(self, path_one, path_two):
-        self.path_one = path_one 
-        self.path_two = path_two
-
-
-class Sorcerer:
-    """
-    Sorcerer class to be used by functions
-    """
-
-    def __init__(self, path_one, path_two):
-        self.path_one = path_one 
-        self.path_two = path_two
+    path_choice = choose_path(character, current_path)
+    roll = dice_roll()
+    final_roll = calculate_final_roll(roll, stats, path_choice)
+    path_divergence(character, final_roll, path_choice, first_path_initiate, second_path_initiate)
 
 
 def read_file(current_story_file):
@@ -76,30 +86,34 @@ def read_file(current_story_file):
     Function to open, read, print and close all 
     story files to feed into other functions.
     """
-
     with open(current_story_file) as file:
         file_text = file.read()
         print(file_text)
 
 
-def choose_path():
+def choose_path(character, current_path):
     """
     Function for choosing path and returning value to functions.
+    Uses current_path to determine user error and return to beginning
+    of current path. All paths require character variable.
     """
-
-    path_choice = input("Please enter your choice here; type the number for the path you want to take:\n")
-    if path_choice == "1" or "2":
-        return path_choice
+    user_choice = input("Please enter your choice here; type the number for the path you want to take:\n")    
+    if user_choice == "1":
+        path = "1"
+    elif user_choice == "2":
+        path = "2"
     else:
         print("Error, you have not chosen one of the options.")
         print("Please try again.\n")
+        current_path(character)
+    
+    return path
 
 
 def dice_roll():
     """
     Dice roll and text to be used in other functions.
     """
-
     roll = random.randrange(1, 20)
     print(roll)
 
@@ -112,7 +126,6 @@ def calculate_final_roll(roll, stats, path_choice):
     Function to determine which stats to use and add
     it to the roll.
     """
-
     if path_choice == "1":
         final_roll = stats.path_one + roll
     elif path_choice == "2":
@@ -128,7 +141,6 @@ def path_divergence(character, final_roll, path_choice, first_path_initiate, sec
     Function for determining which path will be taken
     after dice rolls, to be fed back into main path functions.
     """
-
     if final_roll >= 11 and path_choice == "1":
         print("Your roll succeeds! Forge ahead.\n")
         first_path_initiate(character)
@@ -148,7 +160,6 @@ def reset_game():
     Function for resetting game when the end
     of a path has been reached.
     """
-
     reset = input("Type 'r' to return to the beginning of the game.\n")
     game_reset = reset.lower()
     if game_reset == "r":
@@ -166,7 +177,6 @@ def path_or_bridge(character):
     rolling the dice, calculating the final roll
     and determining the path taken.
     """
-
     if character == "Barbarian":
         stats = Barbarian(2, -2)
         read_file("./assets/story-files/path-or-bridge-barbarian.txt")
@@ -177,20 +187,17 @@ def path_or_bridge(character):
         stats = Sorcerer(2, -2)
         read_file("./assets/story-files/path-or-bridge-sorcerer.txt")
 
+    current_path = path_or_bridge
     first_path_initiate = quest_or_penny
     second_path_initiate = elf_or_friends
     
-    path_choice = choose_path()
-    roll = dice_roll()
-    final_roll = calculate_final_roll(roll, stats, path_choice)
-    path_divergence(character, final_roll, path_choice, first_path_initiate, second_path_initiate)
+    main_game_play(character, stats, current_path, first_path_initiate, second_path_initiate)
 
 
 def elf_or_friends(character):
     """
     Function for path elf_or_friends
     """
-
     if character == "Barbarian":
         stats = Barbarian(-1, 2)
     elif character == "Rogue":
@@ -221,7 +228,6 @@ def friends(character):
     Function for path friend, which goes straight to quest-or-penny
     without rolling.
     """
-
     read_file("./assets/story-files/friends.txt")
     quest_or_penny(character)
 
@@ -230,7 +236,6 @@ def quest_or_penny(character):
     """
     Function for path quest_or_penny
     """
-
     if character == "Barbarian":
         stats = Barbarian(5, -3)
     elif character == "Rogue":
@@ -252,7 +257,6 @@ def dangerous_or_tea(character):
     """
     Function for path dangerous_or_tea
     """
-
     if character == "Barbarian":
         stats = Barbarian(3, -1)
     elif character == "Rogue":
@@ -275,7 +279,6 @@ def penny(character):
     Function for path variant of dangerous_or_tea,
     where you collect an item for later.
     """
-
     if character == "Barbarian":
         stats = Barbarian(3, -1)
     elif character == "Rogue":
@@ -301,7 +304,6 @@ def yes_or_no(character):
     """
     Function for path yes or no.
     """
-
     if character == "Barbarian":
         stats = Barbarian(2, -3)
     elif character == "Rogue":
@@ -323,7 +325,6 @@ def tea(character):
     """
     Function for path tea, which resets game.
     """
-
     read_file("./assets/story-files/tea.txt")
     reset_game()
 
@@ -332,7 +333,6 @@ def north_or_rest(character):
     """
     Function for north or rest.
     """
-
     if character == "Barbarian":
         stats = Barbarian(2, -3)
     elif character == "Rogue":
@@ -355,7 +355,6 @@ def check_inn(character):
     Function for path check_inn, which checks if you received
     the item in path penny and takes the relevant path forward.
     """
-
     read_file("./assets/story-files/check-inn.txt")
 
     if retain_penny is True:
@@ -370,7 +369,6 @@ def sword_or_flamethrower(character):
     """
     Function for path sword or flamethrower.
     """
-
     if character == "Barbarian":
         stats = Barbarian(1, 8)
     elif character == "Rogue":
@@ -392,7 +390,6 @@ def flamethrower(character):
     """
     Function for path flamethrower, which resets game.
     """
-
     read_file("./assets/story-files/flamethrower.txt")
     reset_game()
 
@@ -402,7 +399,6 @@ def barehanded_or_ovenmitts(character):
     Function for path barehanded or ovenmitts.
     Includes text variant for each character.
     """
-
     if character == "Barbarian":
         stats = Barbarian(5, -1)
         read_file("./assets/story-files/barehanded-or-ovenmitts-barbarian-attack.txt")
@@ -426,7 +422,6 @@ def barehanded(character):
     """
     Function for path barehanded, which resets game.
     """
-
     read_file("./assets/story-files/barehanded.txt")
     reset_game()
 
@@ -443,7 +438,6 @@ def attack_or_run(character):
     """
     Function for path attack or run.
     """
-
     if character == "Barbarian":
         stats = Barbarian(6, -6)
     elif character == "Rogue":
@@ -466,7 +460,6 @@ def grab_or_send(character):
     Function for path grab or send.
     Includes text variant for each character.
     """
-
     if character == "Barbarian":
         stats = Barbarian(5, -3)
         read_file("./assets/story-files/grab-or-send-barbarian-attack.txt")
@@ -490,7 +483,6 @@ def run(character):
     """
     Function for path run, which resets game.
     """
-
     read_file("./assets/story-files/run.txt")
     reset_game()
 
@@ -499,7 +491,6 @@ def grab(character):
     """
     Function for path grab, which is a winning path and resets the game.
     """
-
     read_file("./assets/story-files/grab-end.txt")
     reset_game()
 
@@ -508,7 +499,6 @@ def send(character):
     """
     Function for path send, which resets game.
     """
-
     read_file("./assets/story-files/send.txt")
     reset_game()
 
@@ -517,8 +507,7 @@ def main():
     """
     Run all program functions.
     """
-
-    name = user_name()
+    user_name()
     character = choose_character()
     path_or_bridge(character)
 
